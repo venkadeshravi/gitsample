379ffe49a0ecf596ad6bea1c16bb27b5c5163646
venkadeshravi <venkadeshpm28@gmail.com>
2022-10-21 15:27:55 +0530

    completed

---
+++ src/org/git/Git.java
@@ -0,0 +1,16 @@
+package org.git;
+
+public class Git {
+	
+public static void main(String[] args) {
+		
+		int a = 100;
+		
+		System.out.println(a);
+		
+		System.out.println("java");
+		
+
+}
+
+}
